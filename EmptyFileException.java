71d16afac27605e508e5850c247b19e066ada6c6
Mohab Elshabasy <mohabelshabasy@gmail.com>
2025-10-29 15:41:24 -0400

    Lab Commit

--- EmptyFileException.java
+++ EmptyFileException.java
@@ -1,8 +1,10 @@
+import java.io.IOException;
+
 /* this class is meant to flag the exception in case 
  * a wrong file name is provided or its empty */
 
-public class EmptyFileException extends Exception {
+public class EmptyFileException extends IOException {
     public EmptyFileException(String fileName) {
-        super("" + fileName + " was empty");
+        super(fileName + " was empty");
     }
 }
--- WordCounter.java
+++ WordCounter.java
@@ -1,3 +1,7 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -19,81 +23,106 @@ public class WordCounter {
     public static String processText(StringBuffer input, String stopWord)
             throws InvalidStopwordException, TooSmallText {
         String result = "";
-        
-        if (stopWord == null) { // incase that we didnt put a stopword
+
+        if (stopWord == null) {                 // incase that we didnt put a stopword
             result = noStopWord(input);
         } else {
-            result = noStopWord(input);         // checks if text is too small first
+            result = noStopWord(input);         // checks first if the text is too small, if it is throws a new exception
             result = stopWord(input, stopWord); // in the case that we do have a stop word
         }
-        
+
         return result;
     }
 
-    public static StringBuffer processFile(String fileName) throws EmptyFileException {
-        return null;
+    public static StringBuffer processFile(String fileName) throws EmptyFileException, TooSmallText {
+
+        StringBuffer output = new StringBuffer();
+        
+        // want to modify this code so that if the provided fileName is invalid it would prompt us to enter a correct one
+        // also want to raise exceptions when the file is empty or the text in the file is too short 
+        
+        try {
+            
+            LineNumberReader reader = new LineNumberReader(new InputStreamReader(new FileInputStream(fileName)));
+            
+            String line = reader.readLine();
+            
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+            
+            reader.close();
+        
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return output;
     }
 
     public static String userInput() {
-        
+
         System.out.println("enter: ");
-        
+
         Scanner userInput = new Scanner(System.in);
-        
+
         String Input = userInput.nextLine();
-        
+
         userInput.close();
-        
+
         return Input;
     }
 
     public static String stopWord(StringBuffer input, String stopWord) throws InvalidStopwordException {
-        
-        int wordCount = 1;                                              // even if the first word is the stop word the count would still be 1
-        
+
+        int wordCount = 0;
+
         Pattern regex = Pattern.compile("\\b[a-zA-Z0-9']+\\b");
-        
-        Matcher regexMatcher = regex.matcher(input);
-        
-        boolean found = false;
+
+        Matcher regexMatcher = regex.matcher(input);      
+
+        boolean found = false;                          // gets set to true if the stop word is found if not we keep it false
 
         while (regexMatcher.find()) {
             String curWord = regexMatcher.group();
-            if (curWord.equalsIgnoreCase(stopWord)) {
+            
+            if (curWord.equals(stopWord)) {             // breaks as soon as we find the stop word
                 found = true;
+                wordCount++;
                 break;
             }
+            
             wordCount++;
         }
-        
-        if(!found) {
+
+        if (!found) {                   // in the case that we dont find the stop word we raise InvalidStopWord exception
             throw new InvalidStopwordException(stopWord);
         }
-        
+
         String result = "" + wordCount;
-        
+
         return result;
     }
 
     public static String noStopWord(StringBuffer input) throws TooSmallText {
-        
+
         int wordCount = 0;
-        
+
         Pattern regex = Pattern.compile("\\b[a-zA-Z0-9']+\\b");
-        
+
         Matcher regexMatcher = regex.matcher(input);
 
-        while (regexMatcher.find()) {
+        while (regexMatcher.find()) {           // counts all words without a stop word
             wordCount++;
         }
-        
-        
-        if (wordCount < 5 ) {
+
+        if (wordCount < 5) {                    // if the sentence is too short we raise TooSmallText exception
             throw new TooSmallText(wordCount);
         }
-        
+
         String result = "" + wordCount;
-        
+
         return result;
     }
 }
